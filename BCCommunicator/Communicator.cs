d651a681d6babefa6d2acad884b55fbb8dfc4353
Brian Wharton <dafoink@hotmail.com>
2023-07-31 12:32:33 -0400

    Add project files.

---
+++ BCCommunicator/Communicator.cs
@@ -0,0 +1,92 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCCommunicator
+{
+    public class Communicator
+    {
+        /// <summary>
+        /// This will retrieve a new access token, given tenantID, clientID, clientSecret, and the refresh token
+        /// </summary>
+        /// <param name="tenantID">Tenant ID you are using for BC</param>
+        /// <param name="clientID">ClientID setup in Active Directory</param>
+        /// <param name="clientSecret">Client Secret setup in Active Directory</param>
+        /// <param name="refreshToken">Saved refresh token</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public Token RefreshToken(string tenantID, string clientID, string clientSecret, string refreshToken)
+        {
+            // create rest client options and point to the base login.microsoftonline.com
+            var options = new RestClientOptions("https://login.microsoftonline.com")
+            {
+                MaxTimeout = -1,
+            };
+
+            // create a rest client
+            var client = new RestClient(options);
+
+            // we are going to point to the token endpoint (NOTE: the tenantID is part of this endpoint)
+            var request = new RestRequest($"/{tenantID}/oauth2/token", Method.Post);
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+
+            // add the parameters for clientID, secret, granttype (refresh_token), and the refresh_token to use
+            request.AddParameter("client_id", clientID);
+            request.AddParameter("client_secret", clientSecret);
+            request.AddParameter("grant_type", "refresh_token");
+            request.AddParameter("refresh_token", refreshToken);
+
+            // execute the REST call
+            RestResponse response = client.Execute(request);
+
+            // lets do some error handling
+            if (response == null) { throw new Exception("Token Response is Null"); }
+            if (response.Content == null) { throw new Exception("Token Content is null"); }
+            if (response.StatusCode != System.Net.HttpStatusCode.OK) { throw new Exception($"Status not returned as OK ({response.StatusCode}\n{response.Content})"); }
+            Token returnToken = null;
+
+            // serialize the returned token (make sure we do some error handling)
+            try
+            {
+                returnToken = Newtonsoft.Json.JsonConvert.DeserializeObject<Token>(response.Content);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception($"Return Token could not be deserialized\n{ex.Message}");
+            }
+            if(returnToken == null) { throw new Exception("Return Token is null"); }
+
+            // return the token
+            return returnToken;
+        }
+
+
+        /// <summary>
+        /// This is a simple test to test the access token returned when refreshing a token. NOTE:  this is just a simple example that does a GET
+        /// </summary>
+        /// <param name="tenantID">Tenant ID for your BC instance</param>
+        /// <param name="accessToken">Access Token passed back when refreshing a token</param>
+        /// <param name="endpoint">What endpoint do you want to call</param>
+        /// <returns></returns>
+        public string RetrieveData(string tenantID, string accessToken, string endpoint)
+        {
+
+            // base server URL for business central API
+            var options = new RestClientOptions("https://api.businesscentral.dynamics.com")
+            {
+                MaxTimeout = -1,
+            };
+            var client = new RestClient(options);
+            // build total endpoint
+            var request = new RestRequest($"/v2.0/{tenantID}/production/api/v2.0/{endpoint}", Method.Get);
+            request.AddHeader("Authorization", $"Bearer {accessToken}");
+            RestResponse response = client.Execute(request);
+
+            // we are going to pass the response.Content string to the caller
+            return response.Content;
+        }
+    }
+}
---
+++ BCCommunicatorClient/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Runtime.InteropServices;
+
+//create an instance of our library that does the work
+var bcClient = new BCCommunicator.Communicator();
+
+
+// these should come from a config file that is "secure"
+string tenantID = "PUT YOUR TENANT ID HERE -- NOTE: you will probably want this stored in encrypted storage or at least a config file";
+string clientID = "PUT YOUR CLIENT ID HERE -- NOTE: you will probably want this stored in encrypted storage or at least a config file";
+string clientSecret = "PUT YOUR CLIENT SECRET HERE -- NOTE: you will probably want this stored in encrypted storage or at least a config file";
+
+// NOTE:  you will want to store the refresh token for future use
+string refreshToken = "PUT YOUR REFRESH TOKEN HERE -- probably a good idea to store it in a database, etc.";
+
+BCCommunicator.Token currentToken = null;
+
+// NOTE:  if you have a current token, use it. check the expiration date first.  if it is expired.  get a new one
+// you should only have to refresh if you dont have a valid currentToken
+try
+{
+    // call the library call to refresh the token
+    currentToken = bcClient.RefreshToken(
+        tenantID,
+        clientID,
+        clientSecret,
+        refreshToken
+        );
+    // NOTE:  you should store the currentToken token in a database or config file so that you can use it until it expires.
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error refreshing token\n{ex.Message}");
+    // TODO: ErrorHandling
+}
+
+
+
+
+try
+{
+
+    // we are just going to do a simple call to the companies endpoint that will return all companies tied to the tenantID
+    var returnCompanies = bcClient.RetrieveData(tenantID, currentToken.access_token, "companies");
+
+    // we are going to assume it works since the try/catch will catch any errors
+    // we are going to deserialize this as a CompanyCollection class.
+    var returnCompanyCollection = Newtonsoft.Json.JsonConvert.DeserializeObject<BCCommunicator.CompanyCollection>(returnCompanies);
+
+    // lets run through each company and do something simple like print the company name to console.
+    foreach (var company in returnCompanyCollection.value)
+    {
+        Console.WriteLine($"CompanyName = {company.name}");
+    }
+}
+catch(Exception ex)
+{
+    // could be becaused of an expired token, bad data t hat doesnt deserialize to an expected class, or something else.
+    // if expired token, then you should try to refresh the token then try the RetrieveData again
+    Console.WriteLine($"Something happened here.  TODO: Need to have error handling\n{ex.Message}");
+}
+
+
